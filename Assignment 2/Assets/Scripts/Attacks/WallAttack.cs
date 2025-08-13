4d4da537f79260741d8d3d6180208698e32c1609
JesseVolstruis <2597626@students.wits.ac.za>
2025-08-13 17:22:13 +0200

    Added wall attack

---
+++ Assignment 2/Assets/Scripts/Attacks/WallAttack.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class WallAttack : AttackBase
+{
+    [SerializeField]
+    float lifetime = 5f;
+    private Rigidbody2D rb;
+    [SerializeField]
+    private float speed = 5f;
+    private void Start()
+    {
+        hp = 100;
+        cooldown = 5;
+        parryable = false;
+        rb = GetComponent<Rigidbody2D>();
+        StartAttack();
+        Destroy(gameObject, lifetime); //destroy after lifetime
+    }
+
+    protected override void StartAttack()
+    {
+            StartCoroutine(BulletSequence());
+       
+    }
+
+    private IEnumerator BulletSequence()
+    {
+        rb.velocity = Vector2.left * speed;
+
+        yield return new WaitForSeconds(0.01f);
+
+    }
+
+}
