f9052bf7b2cabbfd6bc41c91db80e0cca496eacd
bigchungus <shivolong4@gmail.com>
2025-08-19 22:38:49 +0200

    added keybinds

--- Assignment 2/Assets/Scripts/RocketShield.cs
+++ Assignment 2/Assets/Scripts/RocketShield.cs
@@ -1,18 +1,19 @@
 using UnityEngine;
+using System.Collections;
 
 public class RocketShield : MonoBehaviour
 {
-
     public PlayerHealth playerHealth;
     public PlayerController player;
     public float contactDamage = 50f;   
+    public float invincibilityDelay = 1.2f; // extra time after shield deactivates
+
     private bool isActive = false;
 
     private void Awake()
     {
         gameObject.SetActive(false); 
     }
-
 
     private void OnTriggerEnter2D(Collider2D other)
     {
@@ -26,9 +27,6 @@ public class RocketShield : MonoBehaviour
                 enemyHealth.TakeDamage(contactDamage);
                 Debug.Log("Rocket shield hit enemy!");
             }
-
-
-
         }
         
         if (player.isTransformed && other.CompareTag("Enemy"))
@@ -45,12 +43,19 @@ public class RocketShield : MonoBehaviour
         isActive = true;
     }
 
-    // Call to disable shield
+    // Call to disable shield (with delayed invincibility off)
     public void Deactivate()
     {
-        gameObject.SetActive(false);
-        playerHealth.DisableInvincibility();
+
         isActive = false;
+        StartCoroutine(DisableInvincibilityAfterDelay());
+        gameObject.SetActive(false);
+    }
+
+    private IEnumerator DisableInvincibilityAfterDelay()
+    {
+        yield return new WaitForSeconds(invincibilityDelay);
+        playerHealth.DisableInvincibility();
     }
 }
 
